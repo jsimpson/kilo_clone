6e549b5880a92a0261fc5cf44673164ed9c408d4
Jonathan Simpson <jsimpson.github@gmail.com>
2018-04-18 21:25:07 -0700

    handle movement keys

--- kilo.c
+++ kilo.c
@@ -19,12 +19,25 @@ struct abuf
 
 #define ABUF_INIT { NULL, 0 }
 
+enum key {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN
+};
+
 void die(const char * s);
 
 void enable_raw_mode(void);
 void disable_raw_mode(void);
 
-char read_key(void);
+int read_key(void);
+void move_cursor(int key);
 void process_keypress(void);
 
 int get_cursor_position(int * rows, int * cols);
@@ -40,6 +53,7 @@ void init_editor(void);
 
 struct config
 {
+    int cx,cy;
     int screen_rows;
     int screen_cols;
     struct termios orig_termios;
@@ -90,7 +104,7 @@ enable_raw_mode(void)
     }
 }
 
-char
+int
 read_key(void)
 {
     int nread;
@@ -103,19 +117,149 @@ read_key(void)
         }
     }
 
+    if (c == '\x1b')
+    {
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+        {
+            return '\x1b';
+        }
+
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+        {
+            return '\x1b';
+        }
+
+        if (seq[0] == '[')
+        {
+            if (seq[1] >= '0' && seq[1] <= '9')
+            {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                {
+                    return '\x1b';
+                }
+
+                if (seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                        case '1':
+                            return HOME_KEY;
+                        case '3':
+                            return DEL_KEY;
+                        case '4':
+                            return END_KEY;
+                        case '5':
+                            return PAGE_UP;
+                        case '6':
+                            return PAGE_DOWN;
+                        case '7':
+                            return HOME_KEY;
+                        case '8':
+                            return END_KEY;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                    case 'A':
+                        return ARROW_UP;
+                    case 'B':
+                        return ARROW_DOWN;
+                    case 'C':
+                        return ARROW_RIGHT;
+                    case 'D':
+                        return ARROW_LEFT;
+                }
+            }
+        }
+        else if (seq[0] == 'O')
+        {
+            switch (seq[1])
+            {
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
+            }
+        }
+
+        return '\x1b';
+    }
+    else
+    {
+        return c;
+    }
+
     return c;
+}
+
+void
+move_cursor(int key)
+{
+    switch (key)
+    {
+        case ARROW_LEFT:
+            if (E.cx != 0)
+            {
+                E.cx--;
+            }
+            break;
+        case ARROW_RIGHT:
+            if (E.cx != E.screen_cols - 1)
+            {
+                E.cx++;
+            }
+            break;
+        case ARROW_UP:
+            if (E.cy != 0)
+            {
+                E.cy--;
+            }
+            break;
+        case ARROW_DOWN:
+            if (E.cy != E.screen_rows - 1)
+            {
+                E.cy++;
+            }
+            break;
+    }
 }
 
 void
 process_keypress(void)
 {
-    char c = read_key();
+    int c = read_key();
     switch (c)
     {
         case CTRL_KEY('q'):
             write(STDIN_FILENO, "\x1b[2J", 4);
             write(STDIN_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screen_cols - 1;
+            break;
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screen_rows;
+                while (times--)
+                {
+                    move_cursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+                }
+            }
+      break;
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            move_cursor(c);
             break;
     }
 }
@@ -255,6 +399,10 @@ refresh_screen(void)
 
     draw_rows(&ab);
 
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    ab_append(&ab, buf, strlen(buf));
+
     ab_append(&ab, "\x1b[H", 3);
     ab_append(&ab, "\x1b[?25h", 6);
 
@@ -266,6 +414,9 @@ refresh_screen(void)
 void
 init_editor(void)
 {
+    E.cx = 0;
+    E.cy = 0;
+
     if (get_window_size(&E.screen_rows, &E.screen_cols) == -1)
     {
         die("get_window_size");
